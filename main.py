455e89d0137e380acdc9fe34790b63a825a0d6d8
henryauyong <26832631+henryauyong@users.noreply.github.com>
2021-12-26 19:48:59 +0800

    Finished basic function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from os import remove
+import os
 from pytube import YouTube
 from pytube.streams import Stream
 from pytube import Playlist
@@ -8,6 +8,7 @@ from tkinter import N, W, E, S
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+import ffmpeg
 
 # video: Video object
 # videos: list of Video objects
@@ -17,7 +18,7 @@ videos = []
 
 # todo
 # dropdown menu for resolution & format
-# 1080p download
+# 1080p download (Finished, bitch)
 # make download a method of Video,
 # and add a button to download only the current selected vid
 
@@ -29,22 +30,17 @@ class Video:
             self.title = self.yt.title
             self.thumbnailUrl = self.yt.thumbnail_url
             self.thumbnail = self.getThumbnail()
-            self.format = "mp4"
             self.availableResolution = []
             availableMp4 = self.yt.streams.filter(file_extension="mp4", type="video")
             for i in availableMp4:
-                if i.resolution not in self.availableResolution:
+                if int(i.resolution[:-1]) not in self.availableResolution and i.resolution != None:
                     self.availableResolution.append(int(i.resolution[:-1])) # trim of trailing p for sorting
                     self.availableResolution.sort()
-            self.resolution = str(self.availableResolution[-1]) + "p"
-            self.stream = self.yt.streams.filter(resolution=self.resolution, file_extension=self.format)[0]
         except Exception as e:
             print(e)
             self.yt = None
             self.title = "[invalid video]"
             self.thumbnailUrl = None
-            self.format = None
-            self.resolution = None
             self.availableResolution = None
             availableMp4 = None
             self.stream = None
@@ -59,11 +55,37 @@ class Video:
 def download():
     global status
     for video in videos:
-        try:
-            stream = video.stream
-            stream.download(output_path='video')
-        except Exception as e:
-            print(e)
+        audio = video.yt.streams.filter(type="audio")[0]
+        if(downloadFormat.get() == "video"):
+            res = 720
+            for i in resolutionList:
+                if int(i[:-1]) <= int(str(resolutionListvar.get())[:-1]) and int(i[:-1]) in video.availableResolution:
+                    res = int(i[:-1])
+                    break
+            print(video.availableResolution)
+            stream = video.yt.streams.filter(resolution=str(res) + "p", file_extension="mp4")[0]
+
+            if(res >= 1080):
+                try:
+                    stream.download(output_path='video', filename='tempVideo.mp4')
+                    audio.download(output_path='video', filename='tempAudio.mp4')
+                    input_video = ffmpeg.input('video/tempVideo.mp4')
+                    input_audio = ffmpeg.input('video/tempAudio.mp4')
+                    ffmpeg.concat(input_video, input_audio, v=1, a=1).output(filename="./video/"+str(video.title)+".mp4").run()
+                    os.remove('./video/tempVideo.mp4')
+                    os.remove('./video/tempAudio.mp4') 
+                except Exception as e:
+                    print(e)
+            else:
+                try:
+                    stream.download(output_path='video')
+                except Exception as e:
+                    print(e)
+        else:
+            try:
+                audio.download(output_path='video')
+            except Exception as e:
+                print(e)
     status.config(text="Download completed! check video/")
 
 def addEntry():
@@ -101,15 +123,20 @@ def openEdit(*args):
     thumbnailLabel.image = videos[selection].thumbnail
 
 def main():
-    global frame, inputname, status, urlVar, videoList, videoListvar, videoListbox, thumbnailLabel
+    global frame, inputname, status, urlVar, videoList, videoListvar, videoListbox, thumbnailLabel, resolutionListvar, resolutionList, downloadFormat
 
     # window configuration
     window = tkinter.Tk()
     window.title("YouTube Downloader")
+
     frame = ttk.Frame(window, borderwidth=2, relief="sunken")
     editFrame = ttk.Frame(frame, borderwidth=2, relief="sunken")
+    optionFrame = ttk.Frame(frame, borderwidth=2, relief="sunken")
+
     frame.grid(column=0, row=0, sticky=(N, W, E, S))
-    editFrame.grid(column=4, row=1, sticky=(N, E, S, W))
+    editFrame.grid(column=4, row=1, sticky=(N, E, W))
+    optionFrame.grid(column=4, row=2, sticky=(N, W, E, S))
+
     window.columnconfigure(0, weight=1)
     window.rowconfigure(0, weight=1)
 
@@ -141,21 +168,32 @@ def main():
     # thumbnail label
     thumbnailLabel = tkinter.Label(editFrame, text="Please select a video",image="")
 
+    # dropdown manu for resolution and format choices
+    resolutionList = ["2160p", "1440p", "1080p", "720p", "480p", "360p", "240p", "144p"]
+    resolutionListvar = tkinter.StringVar(value="720p")
+    resolutionListoption = tkinter.OptionMenu(optionFrame, resolutionListvar, *resolutionList)
+
+    # radiobutton for choosing download download format
+    downloadFormat = tkinter.StringVar(value="video")
+    videoButton = tkinter.Radiobutton(optionFrame, text="Video", variable=downloadFormat, value="video")
+    audioButton = tkinter.Radiobutton(optionFrame, text="Audio", variable=downloadFormat, value="audio")
+
     # widgets grid configuration
     inputname.grid(column=1, row=0, sticky=(W, E))
-    videoListbox.grid(column=0, row=1, columnspan=3, sticky=(N, E, S, W))
-    scrollbar.grid(column=3, row=1, sticky=(N, S, W))
+    videoListbox.grid(column=0, row=1, rowspan=2, columnspan=3, sticky=(N, E, S, W))
+    scrollbar.grid(column=3, row=1, rowspan=2, sticky=(N, S, W))
     inputbutton.grid(column=2, row=0, sticky=(W))
     removebutton.grid(column=3, row=0, sticky=())
-    status.grid(column=0, row=2)
-    downloadButton.grid(column=4, row=2)
-    thumbnailLabel.grid(column=0, row=0, columnspan=2)
+    status.grid(column=0, row=3)
+    downloadButton.grid(column=4, row=3)
+    thumbnailLabel.grid(column=0, row=0)
+    resolutionListoption.grid(column=0, row=0)
+    videoButton.grid(column=1, row=0)
+    audioButton.grid(column=1, row=1)
 
     # frame configuration
     frame.columnconfigure(1, weight=1)
-    frame.columnconfigure(4, weight=1)
-    frame.rowconfigure(1, weight=1)
-    editFrame.columnconfigure(0, weight=1)
+    frame.rowconfigure(2, weight=1)
 
     window.mainloop()
 
