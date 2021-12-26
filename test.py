2ec1c2a0361c1ce612d140f4f621a8a1b2b39a32
henryauyong <26832631+henryauyong@users.noreply.github.com>
2021-12-26 20:14:51 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import ffmpeg
-
-input_video = ffmpeg.input('input_video.mp4')
-
-input_audio = ffmpeg.input('input_audio.mp4')
-
-ffmpeg.concat(input_video, input_audio, v=1, a=1).output('finished_video.mp4').run()
